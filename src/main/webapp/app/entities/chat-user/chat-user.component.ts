d73ad9d9f70e20917f93f14218be23e4d92e2982
Tonterias <ecorreos@htomail.com>
2019-09-16 19:26:59 +0200

    Chat user only view menu

--- src/main/webapp/app/entities/chat-user/chat-user.component.ts
+++ src/main/webapp/app/entities/chat-user/chat-user.component.ts
@@ -16,6 +16,8 @@ export class ChatUserComponent implements OnInit, OnDestroy {
   chatUsers: IChatUser[];
   currentAccount: any;
   eventSubscriber: Subscription;
+  owner: any;
+  isAdmin: boolean;
 
   constructor(
     protected chatUserService: ChatUserService,
@@ -44,8 +46,39 @@ export class ChatUserComponent implements OnInit, OnDestroy {
     this.loadAll();
     this.accountService.identity().then(account => {
       this.currentAccount = account;
+      this.owner = account.id;
+      this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
     });
     this.registerChangeInChatUsers();
+  }
+
+  myChatUser() {
+    const query = {
+      //          page: this.page - 1,
+      //          size: this.itemsPerPage,
+      //          sort: this.sort()
+    };
+    query['userId.equals'] = this.owner;
+    this.chatUserService
+      .query(query)
+      .pipe(
+        filter((res: HttpResponse<IChatUser[]>) => res.ok),
+        map((res: HttpResponse<IChatUser[]>) => res.body)
+      )
+      .subscribe(
+        (res: IChatUser[]) => {
+          console.log('CONSOLOG: M:myChatUser & O: query : ', query);
+          console.log('CONSOLOG: M:myChatUser & O: res : ', res);
+          this.chatUsers = res;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    //      this.chatUserService
+    //          .query(query)
+    //          .subscribe(
+    //              (res: HttpResponse<IChatUser[]>) => this.paginatePosts(res.body, res.headers),
+    //              (res: HttpErrorResponse) => this.onError(res.message)
+    //          );
   }
 
   ngOnDestroy() {
@@ -67,6 +100,13 @@ export class ChatUserComponent implements OnInit, OnDestroy {
   registerChangeInChatUsers() {
     this.eventSubscriber = this.eventManager.subscribe('chatUserListModification', response => this.loadAll());
   }
+
+  //  protected paginatePosts(data: IChatUser[], headers: HttpHeaders) {
+  //      this.links = this.parseLinks.parse(headers.get('link'));
+  //      this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+  //      this.posts = data;
+  //      //        console.log('CONSOLOG: M:ngOnInit & O: this.posts : ', this.posts);
+  //  }
 
   protected onError(errorMessage: string) {
     this.jhiAlertService.error(errorMessage, null, null);
